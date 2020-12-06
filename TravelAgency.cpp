3a83f0da1d260445a02692f25d135a6befae35e1
adaneadgo <Edgit132>
2020-12-06 17:32:59 +0200

    עדכון קובץ
    עובד על מייק אורדר וכל היוצא בזה

--- TravelAgency.cpp
+++ TravelAgency.cpp
@@ -1,6 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<ctime>
+
+
 using namespace std;
 
 // Enums: states/type of objects
@@ -13,8 +17,8 @@ typedef struct User { int id = 0; string password; string userName; UserType typ
 typedef struct Date { int day = 0; int month = 0; int year = 0; }Date; // date libary **
 typedef struct Flight { string destanion = "none"; Date out; Date in; }Flight;
 typedef struct Hotel { string name = "none"; string addres = "none"; }Hotel;
-typedef struct Package { int id; Flight f; Hotel h; float rate; int numOfRates; float price; int quantity; } Package;
-typedef struct Order { Date d; int packageId; Status s = in_process; int clientId; int agentId = 0; } Order; // redfine dfd - order approved only if agent accept
+typedef struct Package { int id=0; Flight f; Hotel h; float rate = 0 ; int numOfRates = 0; float price = 0; int quantity = 0; } Package;
+typedef struct Order { Date date; int packageId; Status status = in_process; int clientId; int agentId = 0; } Order; // redfine dfd - order approved only if agent accept
 // other structs
 typedef struct Request { int senderId; int agentId = 0; string content; } Request;
 typedef struct Message { int senderId; int sentId; string content; }Message;
@@ -27,19 +31,25 @@ istream& operator>>(ifstream& f, UserType& u);
 //read/write to/from console
 ostream& operator<<(ostream& os, User& u);
 istream& operator>>(istream& f, User& u);
+// Order
+ostream& operator<<(ostream& os, Order& o);
 
 // Features: 1.
+bool logOrRegist();
 bool userRegistration(UserType t);
 bool login();
 //2.
-bool makeAnOrder();
+bool makeAnOrder(Package p);
+//3.
+bool search();
 
 // Asisnt function: files
 bool writeNewUserToFile(User& newClient);
-User* readUserFromFile(ifstream& f);
 void skipLines(ifstream& f, int n);
 //others
-string strUserType(User& u);
+string strUserType(UserType& u);
+string strStatus(Status& s);
+Date today();
 
 
 User* user = nullptr; // The global logged user
@@ -48,35 +58,62 @@ User* user = nullptr; // The global logged user
 //Main
 int main()
 {
-	
-	login();
-	cout << *user;
-	
+	search();
+
+
 }
 
 
 
-string strUserType(User& u)
+string strUserType(UserType& t)
 {
-	switch (u.type)
+	switch (t)
 	{
 	case client:
 		return "Client";
 	case agent:
 		return "Agent";
 	case manager:
-		return "Manager";
+		return "The Manager";
+	default:
+		return "";
+	}
+}
+string strStatus(Status& s)
+{
+	switch (s)
+	{
+	case Canceled:
+		return "Canceled";
+	case in_process:
+		return "in process";
+	case Confirmed:
+		return "Confirmed";
 	default:
 		return "";
 	}
 }
 
-User* readUserFromFile(ifstream& f)
+Date today()
 {
-	User* user = new User;
-	f >> *user;
-	return user;
+	time_t t = time(NULL);
+	tm time = *localtime(&t);
+	Date d = { time.tm_mday,time.tm_mon + 1,time.tm_year + 1900 };
+	return d;
 }
+bool isDateVaild(Date d)
+{
+	Date dn = today();
+	if (d.year < dn.year)
+		return false;
+	else if (d.month < dn.month)
+		return false;
+	else if(d.day < dn.day)
+		return false;
+
+	return true;
+}
+
 bool writeNewUserToFile(User& newClient)
 {
 	ofstream f("UsersDB.txt",ios::app);
@@ -90,15 +127,25 @@ void skipLines(ifstream& f, int n)
 		f.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
-bool makeAnOrder()
-{
-	// here we need to call the search function that will return the desierd package id
-    // int packageId = searchPackage()
-	int packId = 9999; // example...
 
-	
-	Order order = { Date(), packId, Status::in_process, user->id, 0 };
-	return 1;
+bool logOrRegist()
+{
+
+	int input;
+	do {
+		cout << "please select:" << endl;
+		cout << "1. Login" << endl << "2. Regist" << endl;
+		cin >> input;
+
+		if (input == 1)
+			login();
+		if (input == 2)
+			userRegistration(client);
+
+	} while (input != 1 && input != 2);
+
+	return user != nullptr;
+
 }
 bool userRegistration(UserType t)
 {
@@ -112,7 +159,7 @@ bool userRegistration(UserType t)
 	writeNewUserToFile(*newClient);
 
 	//login
-	if (user)
+	if (!user)
 		user = newClient;
 
 	//if everything went well
@@ -141,6 +188,27 @@ bool login()
 	delete user;
 	return false;
 }
+bool search()
+{
+	// search package need to writen.
+
+	Package p;
+	makeAnOrder(p);
+	return true;
+
+}
+bool makeAnOrder(Package p)
+{
+	if (!user)
+	{ cout << "not logged" << endl; logOrRegist(); }
+	
+	Order order = { today(), p.id, in_process, user->id, 0 };
+	cout << endl << "Summary: " << order << endl << "{package details}" << endl;
+	
+	ofstream f("Orders");
+
+	return 1;
+}
 
 ostream& operator<<(ofstream& f, User& u)
 {
@@ -158,16 +226,9 @@ istream& operator>>(ifstream& f, User& u)
 	f >> u.type;
 	return f;
 }
-istream& operator>>(ifstream& f, UserType& u)
-{
-	int i;
-	f >> i;
-	u = UserType(i);
-	return f;
-}
 ostream& operator<<(ostream& os, User& u)
 {
-	os << endl << strUserType(u);
+	os << endl << strUserType(u.type);
 	os << " Name: " << u.userName << endl;
 	os << "Id: " << u.id << endl;
 	return os;
@@ -182,6 +243,33 @@ istream& operator>>(istream& is, User& u)
 	cout << "UserName: ";
 	is >> u.userName;
 	return is;
+}
+
+istream& operator>>(ifstream& f, UserType& u)
+{
+	int i;
+	f >> i;
+	u = UserType(i);
+	return f;
+}
+
+ostream& operator<<(ostream& os, Order& o)
+{
+	os << "Date: " << "dd.mm.yyyy"<< endl;
+	os << "Client Id: " << o.clientId << endl;
+	os << o.packageId << endl;
+	os << "Order status: " << strStatus(o.status) << endl;
+	return os;
+		
+}
+ostream& operator<<(ofstream& f, Order& o)
+{
+	f << "dd.mm.yyyy" << endl;
+	f << o.clientId << endl;
+	f << "package details" << endl;
+	f << o.status << endl;
+	f << o.agentId << endl;
+	return f;
 }
 
 
