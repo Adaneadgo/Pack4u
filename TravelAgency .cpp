d12caa06112480585d472d4ac9079f5fd7e0f75f
coralavital <coralav@ac.sce.ac.il>
2020-12-06 21:41:32 +0200

    Update TravelAgency .cpp

--- TravelAgency .cpp
+++ TravelAgency .cpp
@@ -20,7 +20,6 @@ typedef struct Hotel { string name = "none"; string addres = "none"; }Hotel;
 typedef struct Package { int id = 0; Flight f; Hotel h; float rate = 0; int numOfRates = 0; float price = 0; int quantity = 0; } Package;
 typedef struct Order { Date date; int packageId; Status status = in_process; int clientId; int agentId = 0; } Order; // redfine dfd - order approved only if agent accept
 //Other structs
-
 typedef struct Message { string from; string message; }Message;
 typedef struct Cupon { int cuponCode; float discount; Date expiry; }Cupon; /// date or quantity 
 
@@ -42,6 +41,7 @@ ostream& operator<<(ostream& os, Order& o);
 //Date
 ostream& operator<<(ostream& os, Date& d);
 istream& operator>>(istream& is, Date& d);
+
 //Features: 1.
 bool logOrRegist();
 bool userRegistration(UserType t);
